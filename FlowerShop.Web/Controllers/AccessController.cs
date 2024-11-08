6e8b15bf3ab761dc6e995b53a0f23c85c05fa9c5
Dung Tien The <dungtienthe1920@gmail.com>
2024-11-08 21:59:14 +0700

    add login ui

---
+++ FlowerShop.Web/Controllers/AccessController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlowerShop.Web.Controllers
+{
+    public class AccessController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
--- FlowerShop.Web/Program.cs
+++ FlowerShop.Web/Program.cs
@@ -85,6 +85,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Access}/{action=Login}/{id?}");
 
 app.Run();
