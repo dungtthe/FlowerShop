103cf92b29403e6024336c4aa43c61440a1c8b28
Dung Tien The <dungtienthe1920@gmail.com>
2024-11-19 14:40:43 +0700

    add layout customer

--- FlowerShop.Web/Areas/Customer/Controllers/HomeController.cs
+++ FlowerShop.Web/Areas/Customer/Controllers/HomeController.cs
@@ -23,8 +23,8 @@ namespace FlowerShop.Web.Areas.Customer.Controllers
             bool result = await _appUserService.LoginAsync("1", "1", false);
             if (result)
             {
-                return RedirectToAction("Index", "Home", new { area = "ADMIN" });
-              //  return View();
+                //return RedirectToAction("Index", "Home", new { area = "ADMIN" });
+                return View();
             }
             return Content("Có lỗi xảy ra");
 
---
+++ FlowerShop.Web/wwwroot/customer/js/myjs.js
@@ -0,0 +1,8 @@
+﻿document.querySelectorAll('.dropdown-submenu .dropdown-toggle').forEach(function (element) {
+    element.addEventListener('click', function (e) {
+        var parent = this.parentElement;
+        parent.classList.toggle('open');
+        e.stopPropagation();
+        e.preventDefault();
+    });
+});
