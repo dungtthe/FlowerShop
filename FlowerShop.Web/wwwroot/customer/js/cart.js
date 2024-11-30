8e7786fc37336fcf161bbe3a32f6834d9ec7ca64
Dung Tien The <dungtienthe1920@gmail.com>
2024-11-30 18:13:35 +0700

    Khách hàng:Fix Partial Messagebox

---
+++ FlowerShop.Web/wwwroot/customer/js/cart.js
@@ -0,0 +1,24 @@
+//thêm 1 sản phẩm vào giỏ hàng
+const AddProductToCart = (productId, quantity) => {
+  const uri = `/cart/add?id=${productId}&quantity=${quantity}`;
+  fetchGet(
+    uri,
+    (success) => {
+      if (success.message === "new") {
+        const cartCountSpan = document.getElementById("count-cartdetail");
+        let currentCount = parseInt(cartCountSpan.textContent) || 0;
+        currentCount++;
+        cartCountSpan.textContent = currentCount;
+        showSuccess("Đã thêm sản phẩm mới vào giỏ hàng");
+      } else {
+        showSuccess("Đã thêm số lượng");
+      }
+    },
+    (fail) => {
+      alert(fail.message);
+    },
+    () => {
+      alert("có lỗi xảy ra");
+    }
+  );
+};
--- FlowerShop.Web/wwwroot/customer/js/myjs.js
+++ FlowerShop.Web/wwwroot/customer/js/myjs.js
@@ -8,25 +8,3 @@
       e.preventDefault();
     });
   });
-
-//thêm 1 sản phẩm vào giỏ hàng
-const AddProductToCart = (productId, quantity) => {
-  const uri = `/cart/add?id=${productId}&quantity=${quantity}`;
-  fetchGet(
-    uri,
-    (success) => {
-      if (success.message === "new") {
-        const cartCountSpan = document.getElementById("count-cartdetail");
-        let currentCount = parseInt(cartCountSpan.textContent) || 0;
-        currentCount++;
-        cartCountSpan.textContent = currentCount;
-      }
-    },
-    (fail) => {
-      alert(fail.message);
-    },
-    () => {
-      alert("có lỗi xảy ra");
-    }
-  );
-};
